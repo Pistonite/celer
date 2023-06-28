11f8c1a32beb8ef6aa12f4bd443a7d3751a4623d
Pistonight <zhaoyizhu2000@gmail.com>
2023-06-27 23:14:12 -0700

    clean up

--- web-client/src/core/engine/WorkerMessge.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-/// Worker Message types
-Object.defineProperty(exports, "__esModule", { value: true });
--- web-client/src/core/engine/console.js
+++
@@ -1,5 +0,0 @@
-var console = {
-    log: function (){
-        postMessage({level: "log", payload: Array.prototype.slice.call(arguments), type: "console", source: __plugin_name__ })
-    }
-};
