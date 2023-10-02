5f45308da6ccfbbd23097364d2c86970976bd1de
Pistonight <zhaoyizhu2000@gmail.com>
2023-10-02 11:37:42 -0700

    trying to use JoinSet for unordered iteration

--- web-server/src/boot.rs
+++ web-server/src/boot.rs
@@ -1,8 +1,11 @@
 //! Things to do on server boot
 
+use std::sync::Arc;
 use std::{path::Path};
 
+use tokio::task::JoinSet;
 use tokio::{fs::File, io};
+use tokio::join;
 use tracing::{info, debug};
 
 /// Setup site origin in static html files
@@ -12,20 +15,32 @@ pub async fn setup_site_origin(docs_dir: &Path, app_dir: &Path, origin: &str) ->
         Some(domain) => domain,
         None => origin.strip_prefix("http://").unwrap_or(origin),
     };
-    process_site_origin_for_path(docs_dir, origin, domain).await?;
-    process_site_origin_for_path(app_dir, origin, domain).await?;
+    let (r1, r2) = join!(
+    process_site_origin_for_path(docs_dir, origin, Arc::from(domain)),
+    process_site_origin_for_path(app_dir, origin, Arc::from(domain)));
+    r1?;
+    r2?;
     Ok(())
 }
 
 #[async_recursion::async_recursion]
-async fn process_site_origin_for_path(path: &Path, origin: &str, domain: &str) -> io::Result<()> {
+async fn process_site_origin_for_path(path: &Path, origin: &str, domain: Arc<str>) -> io::Result<()> {
     if path.is_dir() {
         let mut dir = tokio::fs::read_dir(path).await?;
+        let mut join_set = JoinSet::new();
         while let Some(entry) = dir.next_entry().await? {
-            process_site_origin_for_path(entry.path().as_ref(), origin, domain).await?;
+            // let origin = Arc::clone(&origin);
+            let domain = Arc::clone(&domain);
+            let path = entry.path();
+            join_set.spawn( async move {
+                process_site_origin_for_path(path.as_ref(), origin, domain).await
+            });
+        }
+        while let Some(r) = join_set.join_next().await {
+            r?;
         }
     } else if path.extension().map(|ext| ext == "html").unwrap_or(false) {
-        process_site_origin_in_file(path, origin, domain).await?;
+        process_site_origin_in_file(path, &origin, &domain).await?;
     }
     Ok(())
 }
